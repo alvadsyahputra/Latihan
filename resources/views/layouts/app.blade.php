fff17c517ea81734006875058776ab214059500d
Alvad Syahputra <92365414+alvadsyahputra@users.noreply.github.com>
2022-01-20 14:24:00 +0700

    warehouse #2201-3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 </head>
 <body>
  <nav class="navbar navbar-expand p-0">
- <a class="navbar-brand text-center col-xs-12 col-md-3 col-lg-2 mr-0"href="{{route('home')}}"> Wrproject </a>
+ <a class="navbar-brand text-center col-xs-12 col-md-3 col-lg-2 mr-0"href="#"> Wrproject </a>
  <button class="btn btn-link d-block d-md-none" data-toggle="collapse" data-target="#sidebar-nav" role="button" ><span class="oi oi-menu"></span>
  </button>
  </nav>
